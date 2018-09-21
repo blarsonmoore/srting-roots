7b7f5df7be2f5bda91f1b19eb0d1f464f99d4fcf
blarsonmoore <blarsonmoore@gmail.com>
2018-09-20 18:01:26 -0600

    fix

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.end.GOOGLE_CLIENT_SECRET,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.end.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
