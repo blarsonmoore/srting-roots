0e746d06ec7080c57e5df122a224a048ddce6c8f
blarsonmoore <blarsonmoore@gmail.com>
2018-09-27 16:08:50 -0600

    heroku update

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const keys = require('./config/keys');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
 
-
 require('./models/User');
 require('./services/passport');
 
@@ -23,6 +22,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
