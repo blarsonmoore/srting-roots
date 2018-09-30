1c11174941b452d09a8711e61c0873d98999e09f
blarsonmoore <blarsonmoore@gmail.com>
2018-09-29 23:34:07 -0600

    test

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,7 +9,7 @@ module.exports = app => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
-    res.redirect('/surveys');
+    res.redirect('/userprofile');
   });
 
   app.get('/api/logout', (req, res) => {
